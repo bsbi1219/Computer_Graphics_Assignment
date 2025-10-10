e20f78cae1fe55ce5248a7c9cc279645dac824e1
BAESUBEEN <bsbi1219@naver.com>
2025-10-10 17:40:47 +0900

    사분면 선 그리기

---
+++ OpenGL_12/OpenGL_12/소스.cpp
@@ -0,0 +1,201 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <GL/freeglut_ext.h>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+void make_vertexShaders();
+void make_fragmentShaders();
+GLuint make_shaderProgram();
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+void Keyboard(unsigned char key, int x, int y);
+void Mouse(int button, int state, int x, int y);
+void TimerFunction(int value);
+char* filetobuf(const char* file);
+
+random_device rd;
+uniform_real_distribution<float> color(0.0f, 1.0f);
+
+GLint width = 800, height = 800;
+GLuint shaderProgramID;
+GLuint vertexShader;
+GLuint fragmentShader;
+
+GLuint VAO_L, VBO_L;
+
+void makeLine()
+{
+	GLfloat lines[] =
+	{
+		-1.0f, 0.0f,
+		1.0f, 0.0f,
+
+		0.0f, 1.0f,
+		0.0f, -1.0f
+	};
+
+	glGenVertexArrays(1, &VAO_L);
+	glGenBuffers(1, &VBO_L);
+
+	glBindVertexArray(VAO_L);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_L);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(lines), lines, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("Example1");
+	glewExperimental = GL_TRUE;
+	glewInit();
+	makeLine();
+	make_vertexShaders();
+	make_fragmentShaders();
+	shaderProgramID = make_shaderProgram();
+	glutDisplayFunc(drawScene);
+	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(Mouse); 
+	glutMainLoop();
+}
+
+GLvoid drawScene()
+{
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(shaderProgramID);
+	glPointSize(2.0);
+	GLint loc = glGetUniformLocation(shaderProgramID, "u_color");
+	glUniform3f(loc, 0.0f, 0.0f, 0.0f);
+	glBindVertexArray(VAO_L);
+	glDrawArrays(GL_LINES, 0, 4);
+	glBindVertexArray(0);
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+void make_vertexShaders()
+{
+	GLchar* vertexSource;
+	vertexSource = filetobuf("vertex.glsl");
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: vertex shader\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+void make_fragmentShaders()
+{
+	GLchar* fragmentSource;
+	fragmentSource = filetobuf("fragment.glsl");
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		std::cerr << "ERROR: frag_shader\n" << errorLog << std::endl;
+		return;
+	}
+}
+
+GLuint make_shaderProgram()
+{
+	GLint result;
+	GLchar* errorLog = NULL;
+	GLuint shaderID;
+	shaderID = glCreateProgram();
+	glAttachShader(shaderID, vertexShader);
+	glAttachShader(shaderID, fragmentShader);
+	glLinkProgram(shaderID);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	glGetProgramiv(shaderID, GL_LINK_STATUS, &result);
+	if (!result) {
+		glGetProgramInfoLog(shaderID, 512, NULL, errorLog);
+		std::cerr << "ERROR: shader program\n" << errorLog << std::endl;
+		return false;
+	}
+	glUseProgram(shaderID);
+	return shaderID;
+}
+
+char* filetobuf(const char* file)
+{
+	FILE* fptr;
+	long length;
+	char* buf;
+	fptr = fopen(file, "rb"); // Open file for reading
+	if (!fptr) // Return NULL on failure
+		return NULL;
+	fseek(fptr, 0, SEEK_END); // Seek to the end of the file
+	length = ftell(fptr); // Find out how many bytes into the file we are
+	buf = (char*)malloc(length + 1); // Allocate a buffer for the entire length of the file and a null terminator
+	fseek(fptr, 0, SEEK_SET); // Go back to the beginning of the file
+	fread(buf, length, 1, fptr); // Read the contents of the file in to the buffer
+	fclose(fptr); // Close the file
+	buf[length] = 0; // Null terminator
+	return buf; // Return the buffer
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q':
+		exit(0);
+	}
+	glutPostRedisplay();
+}
+
+void TimerFunction(int value)
+{
+	switch (value)
+	{
+	case 1:
+		glutPostRedisplay();
+		glutTimerFunc(16, TimerFunction, 1);
+		break;
+	}
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+	float ox = (float)x / width * 2.0f - 1.0f;
+	float oy = 1.0f - (float)y / height * 2.0f;
+	if ((button == GLUT_LEFT_BUTTON && state == GLUT_DOWN))
+	{
+	}
+	glutPostRedisplay();
+}
