3da85646140007fdf63e936a441910b10bff8f00
BAESUBEEN <bsbi1219@naver.com>
2025-10-14 20:05:07 +0900

    버텍스 셰이더 수정

--- OpenGL_13/OpenGL_13/vertex.glsl
+++ OpenGL_13/OpenGL_13/vertex.glsl
@@ -1,9 +1,7 @@
 #version 330 core
-layout(location = 0) in vec2 position;
-uniform vec2 u_center;
-uniform float u_scale;
+layout (location = 0) in vec3 aPos;
 
-void main() {
-    vec2 scaled = (position - u_center) * u_scale + u_center;
-    gl_Position = vec4(scaled, 0.0, 1.0);
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
 }
