9772940ad424bd8452770333a57f12f2d100c652
BAESUBEEN <bsbi1219@naver.com>
2025-11-19 19:14:18 +0900

    연산자 오버로딩

--- warp_Class/소스.cpp
+++ warp_Class/소스.cpp
@@ -5,4 +5,9 @@ using namespace std;
 int main() {
 // *Special 함수 6가지*
 // 생성자, 소멸자, 복사 생성자, 복사 할당 연산자, 이동 생성자, 이동 할당 연산자
+
+// *연산자(operator) 오버로딩*
+// class의 멤버 함수는 항상 자기 자신(this)을 인자로 가진다
+// (반환값)operator(연산자)(인자) / ex) bool operator<();
+// friend를 전역 함수 앞에 붙이면 함수가 private한 멤버 변수에 접근이 가능해진다
 }
